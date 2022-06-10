4ea0975022f08493351204450ba7f29b7248d9a0
ChenYuQi17068721 <93569906+ChenYuQi17068721@users.noreply.github.com>
2022-06-10 14:43:57 +0800

    add back location since clustering using it

--- R_files/Main.R
+++
@@ -1,9 +0,0 @@
-## Upload the data( qihan)
-
-## EDA( qihan)
-#- make columns based on each variable like PM2.5,PM 10,NO2
-#- time series plot (x is time, Y is each variable,based on city, overall)
-#- see if thereis any missing values, if have use interpolation or forward
-# taget is either pm2.5 or pm10
-  
-## Modelling & Results & Evaluation()
